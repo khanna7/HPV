927fba7c764b79f238aeed836a545116f2b4b84e
Aditya Khanna <khanna7@uchicago.edu>
2019-12-13 17:53:13 -0600

    refit model 1 with imputed data and using formula() for brevity

--- analysis/model1.R
+++ analysis/model1.R
@@ -3,6 +3,8 @@
 # N.B.  ---------------------------
 # This file uses setup information from "ergm-setup.R"
 
+rm(list=ls())
+
 
 # Libraries ---------------------------
 
@@ -17,188 +19,63 @@ load(file="ergm-setup.RData")
 
 # Fit Model 1 ---------------------------
 
+factors <- c("edges", "degree(0:1)", "gwesp(1, fixed=TRUE)",
+             "nodematch('HIV')", "nodematch('fta')", 
+             "nodefactor('num_condomless_anal_sex_receptive_w1')"
+             ) #model with degree(1) only does not converge
+form.model1 <- as.formula(paste0("hpv_net~", paste0(factors, collapse="+")))
+
 # Terms to include:
 # HPV-16, HPV-18, HPV-31, HPV-33, HPV-35, HPV-45, HPV-51, HPV-52
 # HPV-58, HPV-59, HPV-68, HIV, FTA, Number of receptive anal sex partners
 # Age in years
 
-model_1.16 <- ergm(hpv_net ~ 
-                     edges +
-                     nodematch("hr16")+
-                     # nodematch("hr18")+
-                     # nodematch("hr31")+
-                     # nodematch("hr33")+
-                     # nodematch("hr35")+
-                     # nodematch("hr45")+
-                     # nodematch("hr51")+
-                     # nodematch("hr52")+
-                     # nodematch("hr58")+
-                     # nodematch("hr59")+
-                     # nodematch("hr68")+
-                     nodematch("HIV")+
-                     nodematch("fta")+
-                     nodefactor("num_condomless_anal_sex_receptive_w1")+
-                     gwesp(1, fixed=TRUE)+
-                     #degree(0)+
-                     degree(1)
-)
+model_1.16 <- update(form.model0, ~. +nodematch("hr16"))
+fit.model_1.16 <- ergm(model_1.16, eval.loglik = FALSE)  
+summary(fit.model_1.16)
 
-#model_1.16
-summary(model_1.16)
+model_1.18 <- update(form.model0, ~. +nodematch("hr18"))
+fit.model_1.18 <- ergm(model_1.18, eval.loglik = FALSE)  
+summary(fit.model_1.18)
 
+model_1.31 <- update(form.model0, ~. +nodematch("hr31"))
+fit.model_1.31 <- ergm(model_1.31, eval.loglik = FALSE)  
+summary(fit.model_1.31)
 
-model_1.18 <- ergm(hpv_net ~ 
-                     edges +
-                     nodematch("hr18")+
-                     nodematch("HIV")+
-                     nodematch("fta")+
-                     nodefactor("num_condomless_anal_sex_receptive_w1")+
-                     gwesp(1, fixed=TRUE)+
-                     #degree(0)+
-                     degree(1)
-)
+model_1.33 <- update(form.model0, ~. +nodematch("hr33"))
+fit.model_1.33 <- ergm(model_1.33, eval.loglik = FALSE)  
+summary(fit.model_1.33)
 
-#model_1.18
-summary(model_1.18)
+model_1.35 <- update(form.model0, ~. +nodematch("hr35"))
+fit.model_1.35 <- ergm(model_1.35, eval.loglik = FALSE)  
+summary(fit.model_1.35)
 
-model_1.31 <- ergm(hpv_net ~ 
-                     edges +
-                     nodematch("hr31")+
-                     nodematch("HIV")+
-                     nodematch("fta")+
-                     nodefactor("num_condomless_anal_sex_receptive_w1")+
-                     gwesp(1, fixed=TRUE)+
-                     #degree(0)+
-                     degree(1)
-)
+model_1.39 <- update(form.model0, ~. +nodematch("hr39"))
+fit.model_1.39 <- ergm(model_1.39, eval.loglik = FALSE)  
+summary(fit.model_1.39)
 
-#model_1.31
-summary(model_1.31)
+model_1.45 <- update(form.model0, ~. +nodematch("hr45"))
+fit.model_1.45 <- ergm(model_1.45, eval.loglik = FALSE)  
+summary(fit.model_1.45)
 
-model_1.33 <- ergm(hpv_net ~ 
-                     edges +
-                     nodematch("hr33")+
-                     nodematch("HIV")+
-                     nodematch("fta")+
-                     nodefactor("num_condomless_anal_sex_receptive_w1")+
-                     gwesp(1, fixed=TRUE)+
-                     #degree(0)+
-                     degree(1)
-)
+model_1.51 <- update(form.model0, ~. +nodematch("hr51"))
+fit.model_1.51 <- ergm(model_1.51, eval.loglik = FALSE)  
+summary(fit.model_1.51)
 
-#model_1.33
-summary(model_1.33)
+model_1.52 <- update(form.model0, ~. +nodematch("hr52"))
+fit.model_1.52 <- ergm(model_1.52, eval.loglik = FALSE)  
+summary(fit.model_1.52)
 
-model_1.35 <- ergm(hpv_net ~ 
-                     edges +
-                     nodematch("hr35")+
-                     nodematch("HIV")+
-                     nodematch("fta")+
-                     nodefactor("num_condomless_anal_sex_receptive_w1")+
-                     gwesp(1, fixed=TRUE)+
-                     #degree(0)+
-                     degree(1)
-)
+model_1.58 <- update(form.model0, ~. +nodematch("hr58"))
+fit.model_1.58 <- ergm(model_1.58, eval.loglik = FALSE)  
+summary(fit.model_1.58)
 
-#model_1.35
-summary(model_1.35)
+model_1.59 <- update(form.model0, ~. +nodematch("hr59"))
+fit.model_1.59 <- ergm(model_1.59, eval.loglik = FALSE)  
+summary(fit.model_1.59)
 
-model_1.39 <- ergm(hpv_net ~ 
-                     edges +
-                     nodematch("hr39")+
-                     nodematch("HIV")+
-                     nodematch("fta")+
-                     nodefactor("num_condomless_anal_sex_receptive_w1")+
-                     gwesp(1, fixed=TRUE)+
-                     #degree(0)+
-                     degree(1)
-)
+model_1.68 <- update(form.model0, ~. +nodematch("hr68"))
+fit.model_1.68 <- ergm(model_1.68, eval.loglik = FALSE)  
+summary(fit.model_1.68)
 
-#model_1.39
-summary(model_1.39)
-
-model_1.45 <- ergm(hpv_net ~ 
-                     edges +
-                     nodematch("hr45")+
-                     nodematch("HIV")+
-                     nodematch("fta")+
-                     nodefactor("num_condomless_anal_sex_receptive_w1")+
-                     gwesp(1, fixed=TRUE)+
-                     #degree(0)+
-                     degree(1)
-)
-
-#model_1.45
-summary(model_1.45)
-
-model_1.51 <- ergm(hpv_net ~ 
-                     edges +
-                     nodematch("hr51")+
-                     nodematch("HIV")+
-                     nodematch("fta")+
-                     nodefactor("num_condomless_anal_sex_receptive_w1")+
-                     gwesp(1, fixed=TRUE)+
-                     #degree(0)+
-                     degree(1)
-)
-
-#model_1.18
-summary(model_1.18)
-
-model_1.52 <- ergm(hpv_net ~ 
-                     edges +
-                     nodematch("hr52")+
-                     nodematch("HIV")+
-                     nodematch("fta")+
-                     nodefactor("num_condomless_anal_sex_receptive_w1")+
-                     gwesp(1, fixed=TRUE)+
-                     #degree(0)+
-                     degree(1)
-)
-
-#model_1.52
-summary(model_1.52)
-
-model_1.58 <- ergm(hpv_net ~ 
-                     edges +
-                     nodematch("hr58")+
-                     nodematch("HIV")+
-                     nodematch("fta")+
-                     nodefactor("num_condomless_anal_sex_receptive_w1")+
-                     gwesp(1, fixed=TRUE)+
-                     #degree(0)+
-                     degree(1)
-)
-
-#model_1.58
-summary(model_1.58)
-
-
-model_1.59 <- ergm(hpv_net ~ 
-                     edges +
-                     nodematch("hr59")+
-                     nodematch("HIV")+
-                     nodematch("fta")+
-                     nodefactor("num_condomless_anal_sex_receptive_w1")+
-                     gwesp(1, fixed=TRUE)+
-                     #degree(0)+
-                     degree(1)
-)
-
-#model_1.59
-summary(model_1.59)
-
-model_1.68 <- ergm(hpv_net ~ 
-                     edges +
-                     nodematch("hr68")+
-                     nodematch("HIV")+
-                     nodematch("fta")+
-                     nodefactor("num_condomless_anal_sex_receptive_w1")+
-                     gwesp(1, fixed=TRUE)+
-                     #degree(0)+
-                     degree(1)
-)
-
-#model_1.68
-summary(model_1.68)
 
