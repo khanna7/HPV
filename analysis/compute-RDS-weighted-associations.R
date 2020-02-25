5c2cb53d83399f8f41f7edc2ae9fb3d33702b1d8
Aditya Khanna <khanna7@uchicago.edu>
2020-02-25 17:27:48 -0600

    compute weighted summaries in the entire sample

--- analysis/compute-RDS-weighted-associations.R
+++ analysis/compute-RDS-weighted-associations.R
@@ -24,3 +24,26 @@ rds.dt <- as.data.frame(readRDS("rds.dt.RDS"))
 dt.svydesign <- svydesign(data = rds.dt,
                           ids = ~caseid,
                           weights = ~gile.wt)
+
+
+# Compute weighted summaries (full sample) ---------------------------
+
+svymean(x = ~age_w1, na.rm = TRUE, design = dt.svydesign)
+svymean(x=~hr_hpv_any, na.rm = TRUE, design = dt.svydesign)
+svymean(x=~num_hr_hpv, na.rm = TRUE, design = dt.svydesign)
+svymean(x=~mult_hr_type, na.rm = TRUE, design = dt.svydesign)
+svymean(x=~hpv_16_and_18, na.rm = TRUE, design = dt.svydesign)
+svymean(x=~hpv_16_or_18, na.rm = TRUE, design = dt.svydesign)
+svymean(x=~nine.val.vac, na.rm = TRUE, design = dt.svydesign)
+svymean(x=~hr_16, na.rm = TRUE, design = dt.svydesign)
+svymean(x=~hr_18, na.rm = TRUE, design = dt.svydesign)
+svymean(x=~hr_31, na.rm = TRUE, design = dt.svydesign)
+svymean(x=~hr_33, na.rm = TRUE, design = dt.svydesign)
+svymean(x=~hr_35, na.rm = TRUE, design = dt.svydesign)
+svymean(x=~hr_39, na.rm = TRUE, design = dt.svydesign)
+svymean(x=~hr_45, na.rm = TRUE, design = dt.svydesign)
+svymean(x=~hr_51, na.rm = TRUE, design = dt.svydesign)
+svymean(x=~hr_52, na.rm = TRUE, design = dt.svydesign)
+svymean(x=~hr_58, na.rm = TRUE, design = dt.svydesign)
+svymean(x=~hr_59, na.rm = TRUE, design = dt.svydesign)
+svymean(x=~hr_68, na.rm = TRUE, design = dt.svydesign)
