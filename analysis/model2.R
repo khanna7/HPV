557363c1fd116328e255031afb99797327682396
Aditya Khanna <khanna7@uchicago.edu>
2020-07-10 18:48:57 -0500

    updated model 2

--- analysis/model2.R
+++ analysis/model2.R
@@ -13,12 +13,12 @@ library(ergm)
 
 # Load data ---------------------------
   
-load(file="ergm-setup.RData")
+load(file="ergm-setup-data-delete-24-cases.RData")
 
 
 # Fit Model 2 with HR 16 ---------------------------
 
-model_1.16 <- ergm(hpv_net ~ 
+model_2.16 <- ergm(hpv_net ~ 
                      edges +
                      #nodematch("hr16")+
                      nodematch("hiv_hpv16", diff=T)+
@@ -28,30 +28,33 @@ model_1.16 <- ergm(hpv_net ~
                      gwesp(1, fixed=TRUE)+
                      degree(0)+
                      degree(1),
-                   control = control.ergm(MCMC.interval = 1e4,
-                                          MCMC.samplesize = 1e4)
+                   #control = control.ergm(MCMC.interval = 1e4,
+                    #                      MCMC.samplesize = 1e4),
+                   eval.loglik = F
 )
 
 summary(model_2.16)
 gof.model_2.16 <- gof(model_2.16)
 p.model_2.16 <- plot(gof.model_2.16)
+mcmc.diagnostics(model_2.16)
 
 
 
 # Fit Model 2 with HR 45 ---------------------------
 
-model_1.45 <- ergm(hpv_net ~ 
-                     edges +
-                     #nodematch("hr45")+
-                     nodematch("hiv_hpv45", diff=T)+
-                     #nodematch("HIV")+
-                     nodematch("fta")+
-                     nodefactor("num_condomless_anal_sex_receptive_w1")+
-                     gwesp(1, fixed=TRUE)+
-                     #degree(0)+
-                     degree(1)
-)
-
-summary(model_2.45)
-gof.model_2.45 <- gof(model_2.45)
-p.model_2.45 <- plot(gof.model_2.45)
+# model_2.45 <- ergm(hpv_net ~ 
+#                      edges +
+#                      #nodematch("hr45")+
+#                      nodematch("hiv_hpv45", diff=F)+
+#                      #nodematch("HIV")+
+#                      nodematch("fta")+
+#                      nodefactor("num_anal_sex_receptive_2_w1")+
+#                      gwesp(1, fixed=TRUE)+
+#                      #degree(0)+
+#                      degree(1),
+#                    eval.loglik = F
+# )
+# 
+# summary(model_2.45)
+# gof.model_2.45 <- gof(model_2.45)
+# p.model_2.45 <- plot(gof.model_2.45)
